35145ae6fc97a86b96b3a8e2c5216cc8adebecbf
yosephbernandus <yosephbernandus@gmail.com>
2021-12-04 23:31:13 +0700

    membuat api data types

--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import GameItem from "../../molecules/GameItem";
 import { getFeaturedGame } from "../../../services/player";
+import { GameItemTypes } from "../../../services/data-types";
 
 export default function FeaturedGame() {
     const [gameList, setGameList] = useState([]);
@@ -14,6 +15,7 @@ export default function FeaturedGame() {
         getFeatureGameList()
     }, [])
 
+    const API_IMG = process.env.NEXT_PUBLIC_MG;
     return (
         <section className="featured-game pt-50 pb-50">
             <div className="container-fluid">
@@ -21,11 +23,14 @@ export default function FeaturedGame() {
                 </h2>
                 <div className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
                     data-aos="fade-up">
-                    {gameList.map(item => {
-                        return (
-                            <GameItem key={item._id} title={item.name} category={item.category.name} thumbnail={`https://voucher-store.herokuapp.com/uploads/${item.thumbnail}`} />
-                        )
-                    })}
+                    {gameList.map((item: GameItemTypes) => (
+                        <GameItem
+                            key={item._id}
+                            title={item.name}
+                            category={item.category.name}
+                            thumbnail={`${API_IMG}/${item.thumbnail}`}
+                        />
+                    ))}
                 </div>
             </div>
         </section>
