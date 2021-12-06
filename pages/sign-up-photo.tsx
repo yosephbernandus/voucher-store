226ddd51f196080ed2318330ad379fc0cfe61f50
yosephbernandus <yosephbernandus@gmail.com>
2021-12-06 23:33:44 +0700

    add upload photo and integrasi sign up

--- pages/sign-up-photo.tsx
+++ pages/sign-up-photo.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image"
 import { useCallback, useEffect, useState } from "react"
+import { setSignUp } from "../services/auth";
 import { getGameCategory } from "../services/player"
 
 export default function SignUpPhoto() {
     const [categories, setCategories] = useState([]);
     const [favorite, setFavorite] = useState('');
+    const [image, setImage] = useState('');
+    const [imagePreview, setImagePreview] = useState(null);
+    const [localForm, setLocalForm] = useState({
+        name: '',
+        email: '',
+    });
 
     const getGameCategoryAPI = useCallback(async () => {
         const data = await getGameCategory();
@@ -17,8 +24,30 @@ export default function SignUpPhoto() {
         getGameCategoryAPI();
     }, []);
 
-    const onSubmit = () => {
+    useEffect(() => {
+        const getLocalForm = localStorage.getItem('user-form');
+        setLocalForm(JSON.parse(getLocalForm));
+    }, [])
+
+    const onSubmit = async () => {
         console.log(favorite);
+        console.log(image);
+
+        const getLocalForm = await localStorage.getItem('user-form');
+        const form = JSON.parse(getLocalForm);
+        const data = new FormData();
+        data.append('image', image);
+        data.append('email', form.email);
+        data.append('name', form.name);
+        data.append('password', form.password);
+        data.append('username', form.name);
+        data.append('phoneNumber', '08123456789');
+        data.append('role', 'user');
+        data.append('status', 'Y');
+        data.append('favorite', favorite);
+
+        const result = await setSignUp(data);
+        console.log(result);
     }
     return (
         <section className="sign-up-photo mx-auto pt-lg-227 pb-lg-227 pt-130 pb-50">
@@ -29,13 +58,23 @@ export default function SignUpPhoto() {
                             <div className="mb-20">
                                 <div className="image-upload text-center">
                                     <label htmlFor="avatar">
-                                        <Image src="/icon/upload.svg" width={120} height={120} alt="upload" />
+                                        {imagePreview ? <img src={imagePreview} className="img-upload" alt="upload" /> : <Image src="/icon/upload.svg" width={120} height={120} alt="upload" />}
                                     </label>
-                                    <input id="avatar" type="file" name="avatar" accept="image/png, image/jpeg" />
+                                    <input
+                                        id="avatar"
+                                        type="file"
+                                        name="avatar"
+                                        accept="image/png, image/jpeg"
+                                        onChange={(event) => {
+                                            console.log(event.target.files);
+                                            const img = event.target.files[0];
+                                            setImagePreview(URL.createObjectURL(img))
+                                            return setImage(img);
+                                        }} />
                                 </div>
                             </div>
-                            <h2 className="fw-bold text-xl text-center color-palette-1 m-0">Shayna Anne</h2>
-                            <p className="text-lg text-center color-palette-1 m-0">shayna@anne.com</p>
+                            <h2 className="fw-bold text-xl text-center color-palette-1 m-0">{localForm.name}</h2>
+                            <p className="text-lg text-center color-palette-1 m-0">{localForm.email}</p>
                             <div className="pt-50 pb-50">
                                 <label htmlFor="category" className="form-label text-lg fw-medium color-palette-1 mb-10">Favorite
                                     Game</label>
