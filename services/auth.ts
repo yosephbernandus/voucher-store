09191d17137f49b685433a37d6e168510742a72f
yosephbernandus <yosephbernandus@gmail.com>
2021-12-07 23:59:30 +0700

    refactor api services

--- services/auth.ts
+++ services/auth.ts
@@ -1,42 +1,25 @@
-import axios from "axios";
-import { LoginTypes, RegisterTypes } from "./data-types";
+import callAPI from "../config/api";
+import { LoginTypes } from "./data-types";
 
 const ROOT_API = process.env.NEXT_PUBLIC_API;
 const API_VERSION = 'api/v1';
 
 
-export async function setSignUp(data: RegisterTypes) {
-    const URL = 'auth/signup';
-
-    const response = await axios.post(`${ROOT_API}/${API_VERSION}/${URL}`, data).catch(err => err.response);
-
-    const axiosResponse = response.data;
-
-    if (axiosResponse?.error === 1) {
-        return axiosResponse
-    }
-    return axiosResponse.data;
+export async function setSignUp(data: FormData) {
+    const url = `${ROOT_API}/${API_VERSION}/auth/signup`;
+    return callAPI({
+        url,
+        method: 'POST',
+        data
+    });
 }
 
 export async function setLogin(data: LoginTypes) {
-    const URL = 'auth/signin';
+    const url = `${ROOT_API}/${API_VERSION}/auth/signin`;
 
-    const response = await axios.post(`${ROOT_API}/${API_VERSION}/${URL}`, data).catch(err => err.response);
-
-    if (response.status > 300) {
-        const res = {
-            error: true,
-            message: response.data.message,
-            data: null
-        }
-        return res;
-    }
-
-    const res = {
-        error: false,
-        message: 'success',
-        data: response.data.data
-    }
-
-    return res;
+    return callAPI({
+        url,
+        method: 'POST',
+        data
+    });
 }
