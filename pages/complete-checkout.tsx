139a754b2fa6ca8c70054c44dd164ac99b4698ed
yosephbernandus <yosephbernandus@gmail.com>
2021-12-12 17:56:17 +0700

    fix route setelah checkout

--- pages/complete-checkout.tsx
+++ pages/complete-checkout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import Link from 'next/link';
 
 export default function CompleteCheckout() {
     return (
@@ -127,12 +127,22 @@ export default function CompleteCheckout() {
                         WhatsApp</p>
                 </div>
                 <div className="button-group d-flex flex-column mx-auto">
-                    <a className="btn btn-dashboard fw-medium text-lg text-white rounded-pill mb-16"
-                        href="../member/overview.html" role="button">My
-                        Dashboard</a>
-                    <a className="btn btn-whatsapp fw-medium text-lg color-palette-1 rounded-pill" href="/#"
-                        role="button">WhatsApp
-                        ke Admin</a>
+                    <Link href="/member">
+                        <a
+                            className="btn btn-dashboard fw-medium text-lg text-white rounded-pill mb-16"
+                            role="button"
+                        >
+                            My Dashboard
+                        </a>
+                    </Link>
+
+                    <a
+                        className="btn btn-whatsapp fw-medium text-lg color-palette-1 rounded-pill"
+                        href="https://wa.me/62877881540796?text=Saya%20sudah%20melakukan%20pembayaran"
+                        role="button"
+                    >
+                        WhatsApp ke Admin
+                    </a>
                 </div>
             </div>
         </section>
