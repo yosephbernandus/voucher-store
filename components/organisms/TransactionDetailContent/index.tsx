4b98bb47fc86d0fb4d15c7ecbfefe608971af98e
yosephbernandus <yosephbernandus@gmail.com>
2021-12-18 02:35:03 +0700

    Integrasi Payment Information

--- components/organisms/TransactionDetailContent/index.tsx
+++ components/organisms/TransactionDetailContent/index.tsx
@@ -46,11 +46,11 @@ export default function TransactionDetailContent(props: TransactionDetailContent
                             </div>
                             <div className="payment pt-10 pb-10">
                                 <h2 className="fw-bold text-xl color-palette-1 mb-20">Payment Informations</h2>
-                                <Row label="Your Account Name" value="Masayoshi Angga Zero" />
-                                <Row label="Type" value="Worldwide Transfer" />
-                                <Row label="Bank Name" value="Mandiri" />
-                                <Row label="Bank Account Name" value="PT Store GG Indonesia" />
-                                <Row label="Bank Number" value="1800 - 9090 - 2021" />
+                                <Row label="Your Account Name" value={data.name} />
+                                <Row label="Type" value={data.historyPayment.type} />
+                                <Row label="Bank Name" value={data.historyPayment.bankName} />
+                                <Row label="Bank Account Name" value={data.historyPayment.name} />
+                                <Row label="Bank Number" value={data.historyPayment.noRekening} />
                             </div>
                             <div className="d-md-block d-flex flex-column w-100">
                                 <a className="btn btn-whatsapp rounded-pill fw-medium text-white border-0 text-lg" href="#"
