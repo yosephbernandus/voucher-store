03d5b24de42efebb4f77cbadf96065932c7270a5
yosephbernandus <yosephbernandus@gmail.com>
2021-12-18 18:33:28 +0700

    setup seo nextjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import FeaturedGame from '../components/organisms/FeaturedGame';
 import Reached from '../components/organisms/Reached';
 import Story from '../components/organisms/Story';
 import Footer from '../components/organisms/Footer';
+import Head from 'next/head';
 
 
 export default function Home() {
@@ -15,6 +16,14 @@ export default function Home() {
   }, []);
   return (
     <>
+      <Head>
+        <title>Voucher Store - Get New store game</title>
+        <meta name="description" content="Get new store game" />
+        <meta property="og:title" content="get a new store game" />
+        <meta property="og:description" content="get a new store game" />
+        <meta property="og:image" content="https://test-seo.com" />
+        <meta property="og:url" content="https://voucher-store.com" />
+      </Head>
       <Navbar />
       <MainBanner />
       <TransactionStep />
