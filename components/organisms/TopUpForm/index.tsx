05733ae9e56ce49570bb4bad9778465bd0d8f00d
yosephbernandus <yosephbernandus@gmail.com>
2021-12-18 18:42:47 +0700

    fix eslint import component local error

--- components/organisms/TopUpForm/index.tsx
+++ components/organisms/TopUpForm/index.tsx
@@ -44,7 +44,7 @@ export default function TopUpForm(props: TopUpFormProps) {
         }
     }
     return (
-        <form action="./checkout.html" method="POST">
+        <>
             <div className="pt-md-50 pt-30">
                 <div className="">
                     <label htmlFor="ID" className="form-label text-lg fw-medium color-palette-1 mb-10">Verify ID</label>
@@ -120,6 +120,6 @@ export default function TopUpForm(props: TopUpFormProps) {
                     Continue
                 </button>
             </div>
-        </form>
+        </>
     )
 }
